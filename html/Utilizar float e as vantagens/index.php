9fccb74e02d011deed412d8d17de0db2b201b2f7
LucasPFChiesa <lucas.onehost@gmail.com>
2022-08-25 15:54:38 -0300

    novos exercicios

---
+++ html/Utilizar float e as vantagens/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Título</title>
+        <meta charset="utf-8"/>
+        <link href="style.css" type="text/css" rel="stylesheet"/>    
+    </head>
+    <body>
+        <div class="pai">
+            <div class="child">1</div>
+            <div class="child">2</div>
+            <div class="child">3</div>
+            <div class="child">4</div>
+            <div style="clear:both"></div>    
+        </div><!--pai-->
+    </body>
+</html>
