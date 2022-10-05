023eb29aa741d7017ab35413dbc4300f42d01ce7
LucasPFChiesa <lucas.onehost@gmail.com>
2022-10-05 16:06:44 -0300

    Mais Criações

---
+++ HTML5 e as Novidades/Recriando tela de login do facebook 2/index.php
@@ -0,0 +1,156 @@
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+    <title>Login Facebook</title>
+    <!--Aplicar Meta Tags-->
+    <link rel="stylesheet" href="http://fonts.googleapis.com/css?family=Lato:400,700">
+    <link rel="stylesheet" href="./style.css">
+</head>
+
+<body>
+
+    <header>
+        <div class="center">
+
+            <div class="logo">
+                <h2>facebook</h2>
+            </div>
+
+            <form class="form-login" method="post">
+                <div class="form-element">
+                    <p>Email ou Telefone</p>
+                    <input type="email">
+                </div>
+                <div class="form-element">
+                    <p>Senha</p>
+                    <input type="email">
+                </div>
+                <div class="form-element">
+                    <input type="submit" name="acao" value="Entrar">
+                    <div class="clear"></div>
+                </div>
+            </form>
+            <div class="clear"></div>
+
+        </div>
+    </header>
+
+    <section class="main">
+        <div class="center">
+
+            <div class="img-pessoas">
+                <img src="./img-pessoas.png">
+            </div>
+            <div class="abrir-conta">
+                <h2>Abra sua conta</h2>
+                <h3>É gratuito e sempre será!</h3>
+
+                <form class="create-account">
+                    <div class="w50">
+                        <input type="text" placeholder="nome">
+                    </div>
+                    <div class="w50">
+                        <input type="text" placeholder="sobrenome">
+                    </div>
+                    <div class="w100">
+                        <input type="email" placeholder="e-mail">
+                    </div>
+                    <div class="w100">
+                        <input type="password" placeholder="senha">
+                    </div>
+
+                    <div class="w100">
+                        <h2>Data de Nascimento:</h2>
+                        <select name="nascimento-dia" class="nascimento">
+                            <?php for($i=0;$i<=31;$i++){
+                            <option value="0">1</option>
+                            }?>
+                        </select>
+                        <select class="nascimento-mes" class="nascimento">
+                            <option value="1">Junho</option>
+                        </select>
+                        <select name="nascimento-ano" class="nascimento">
+                            <option value="2">2002</option>
+                        </select>
+                    </div>
+
+                    <div class="w100">
+                        <input type="submit" name="acao" value="Cadastrar">
+                    </div>
+                </form>
+
+            </div>
+            <div class="clear"></div>
+        </div>
+    </section>
+
+
+</body>
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<!--
+
+    <meta charset="utf-8" />
+    <meta name="description" content="Descrição do meu site." />
+    <meta name="keywords" content="palavras,separadas,por,virgula" />
+    <meta name="author" content="Guilherme">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0,maximum-scale=1.0" />
+    <link href="css/style.css" rel="stylesheet" />
+
+-->
